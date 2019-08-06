0ebdd84acab11bd7b7d8a0241b612a074ec555fc
Greg Lindsay <greglin@microsoft.com>
2019-08-06 12:11:57 -0700

    delete script

--- windows/whats-new/metadata.ps1
+++
@@ -1,8 +0,0 @@
-$files = Get-ChildItem . *.md -rec
-foreach ($file in $files)
-{
-    (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "author: greg-lindsay", "audience: itpro`rauthor: greg-lindsay" } |
-    Set-Content $file.PSPath
-    $file.PSPath
-}
